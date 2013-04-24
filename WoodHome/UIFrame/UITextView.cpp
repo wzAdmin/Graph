156daa1e1b09eab3c51b008619be6aed2d2c86b4
woodhome <wudecs2007@126.com>
2013-04-24 23:28:45 +0800

    TextView implement with out wrap

--- WoodHome/UIFrame/UITextView.cpp
+++ WoodHome/UIFrame/UITextView.cpp
@@ -1,7 +1,10 @@
 #include "UITextView.h"
+#include "Graphics.h"
+#include "FontConfig.h"
+#include "SlimXml.h"
+#include "CommonFun.h"
 
-
-CUITextView::CUITextView(void)
+CUITextView::CUITextView(void): mFontID(0)
 {
 }
 
@@ -12,10 +15,13 @@ CUITextView::~CUITextView(void)
 
 void CUITextView::Load( const slim::XmlNode* node )
 {
-
+	CUIObject::Load(node);
+	mText = AnsiToWstring(node->readAttributeAsString("text"));
+	mFontID = node->readAttributeAsInt("font");
 }
 
 void CUITextView::Draw( CGraphics* pGraphic )
 {
 
+	pGraphic->DrawTextW(mText.c_str(),Bound(),*sFontConfig.GetFont(mFontID),CENTER);
 }
--- WoodHome/UIFrame/UITextView.h
+++ WoodHome/UIFrame/UITextView.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <string>
 #include "uiobject.h"
+
 class UI_API CUITextView :
 	public CUIObject
 {
@@ -7,8 +9,14 @@ public:
 	static CUIObject* Create(){return NEW_LEAKCHECK CUITextView;}
 	CUITextView(void);
 	~CUITextView(void);
+public:
+	void Text(const std::wstring& wcs){mText = wcs;}
+	const std::wstring& Text() const {return mText;}
 private:
 	virtual void Load(const slim::XmlNode* node);
 	virtual void Draw(CGraphics* pGraphic);
+private:
+	std::wstring mText;
+	int mFontID;
 };
 
